1d4843f1b6df93ae8a24a51d1db5810dac89b3ea
Leonard Ginters <leonard@linters.de>
2020-02-08 23:30:28 +0100

    Add some better type declaration

--- test/general.ts
+++ test/general.ts
@@ -1,12 +1,12 @@
-import { spawn } from "child_process";
-import ava from "ava";
+import { ChildProcess, spawn } from "child_process";
+import ava, { ExecutionContext } from "ava";
 
-ava("Does Gow create a process?", async test => {
-    let messages = "";
-    const gowProcess = !/^win/.test(process.platform) ? spawn("gow") : spawn("cmd", ["/s", "/c", "gow"]);
-    gowProcess.stdout.on("data", data => messages += data.toString());
+ava("Does Gow create a process?", async (test: ExecutionContext) => {
+    let messages: string = "";
+    const gowProcess: ChildProcess = !/^win/.test(process.platform) ? spawn("gow") : spawn("cmd", ["/s", "/c", "gow"]);
+    gowProcess.stdout.on("data", (data: string) => messages += data.toString());
 
-    await new Promise(resolve => {
+    await new Promise((resolve: any): void => {
         setTimeout(() => {
             if (messages.includes("Created process")) {
                 test.pass("Gow created a process successfully");
